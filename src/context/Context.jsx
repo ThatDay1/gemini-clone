37be5d91a6bc94e6e3c803b7d463067d968bb2a1
ThatDay1 <120332992+ThatDay1@users.noreply.github.com>
2024-03-15 12:36:55 +0300

    prompt in sidebar

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -17,15 +17,27 @@ const ContextProvider = (props) => {
     }, 75 * index)
   }
 
+  const newChat = () => {
+    setLoading(false)
+    setShowResult(false)
+  }
+
   const onSent = async (prompt) => {
     setResultData('')
     setLoading(true)
     setShowResult(true)
-    setRecentPrompt(input)
+    let response
+    if (prompt !== undefined) {
+      response = await runChat(prompt)
+      setRecentPrompt(prompt)
+    } else {
+      setPrevPrompts((prev) => [...prev, input])
+      setRecentPrompt(input)
+      response = await runChat(input)
+    }
 
-    const response = await runChat(input)
     let responseArray = response.split('**')
-    let newResponse
+    let newResponse = ' '
     for (let i = 0; i < responseArray.length; i++) {
       if (i === 0 || i % 2 !== 1) {
         newResponse += responseArray[i]
